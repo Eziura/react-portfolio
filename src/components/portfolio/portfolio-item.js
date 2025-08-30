8613d4b7cf24e6072db11fbd2b9c71b05273984c
Aruize <anruiesc@gmail.com>
2025-08-30 20:48:13 +0200

    event listener blur

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -17,19 +17,24 @@ export default class PortfolioItem extends Component {
   }
 
   handleMouseEnter() {
-
+    this.setState({ portfolioItemClass: "image-blur" })
   }
 
   handleMouseLeave() {
-
+    this.setState({ portfolioItemClass: "" })
   }
 
   render() {
     const { id, description, thumb_image_url, logo_url } = this.props.item;
     return (
-      <div className="portfolio-item-wrapper">
+      <div className="portfolio-item-wrapper"
+        // JS listeners
+        onMouseEnter={() => this.handleMouseEnter()}
+        onMouseLeave={() => this.handleMouseLeave()}
+      >
+
         <div
-          className="portfolio-img-background"
+          className={"portfolio-img-background " + this.state.portfolioItemClass}
           style={{
             backgroundImage: "url(" + thumb_image_url + ")"
           }}
