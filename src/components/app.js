3ce17e49aed2d2b40a01c6f1a15d645910a8f453
Aruize <anruiesc@gmail.com>
2025-09-15 20:36:47 +0200

    blog page creation

--- src/components/app.js
+++ src/components/app.js
@@ -10,6 +10,7 @@ import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
+import BlogDetail from "./pages/blog-detail";
 import PortfolioManager from "./pages/portfolio-manager";
 import PortfolioDetail from "./portfolio/portfolio-detail";
 import Auth from "./pages/auth";
@@ -118,6 +119,7 @@ export default class App extends Component {
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
+              <Route path="/b/:slug" component={BlogDetail} />
 
               {/* Estas paginas solo son visibles si estas logueado y se colocan en la lista de authorizedPages */}
               {this.state.loggedInStatus === "LOGGED_IN" ? this.authorizedPages() : null}
--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,14 +1,78 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-export default function () {
-    return (
-        <div>
-            <h2>Blog</h2>
+import BlogItem from "../blog/blog-item";
 
-            <div>
-                <Link to="/about-me">Read more about myself</Link>
+class Blog extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            blogItems: [],
+            totalCount: 0,
+            currentPage: 0
+        }
+
+        this.getBlogItems = this.getBlogItems.bind(this);
+        this.activateInfiniteScroll();
+    }
+
+    activateInfiniteScroll() {
+        window.onscroll = () => {
+            console.log("window.innerHeight", window.innerHeight);
+            console.log(
+                "document.documentElement.scrollTop",
+                document.documentElement.scrollTop
+            );
+            console.log(
+                "document.documentElement.offsetHeight",
+                document.documentElement.offsetHeight
+            );
+
+            if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
+                console.log("end of page");
+            }
+        };
+
+    }
+
+    getBlogItems() {
+        this.setState({
+            currentPage: this.state.currentPage + 1
+        })
+        axios.get('https://eziura.devcamp.space/portfolio/portfolio_blogs',
+            { withCredentials: true }
+        ).then(response => {
+            this.setState({
+                blogItems: response.data.portfolio_blogs
+            })
+        }).catch(error => {
+            console.log('error', error);
+        })
+
+    }
+
+    componentDidMount() {
+        this.getBlogItems();
+    }
+
+    render() {
+
+        const blogRecords = this.state.blogItems.map(blogItem => {
+            return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+        })
+
+        return (
+            <div className="blog-container">
+                <div className="content-container">
+                    {blogRecords}
+                </div>
             </div>
-        </div>
-    );
+        );
+
+
+    }
 }
+export default Blog;
+
